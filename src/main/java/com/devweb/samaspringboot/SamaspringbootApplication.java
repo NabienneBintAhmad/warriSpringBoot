a2674abb37be4806ac8b17242815fcb8a91bb34e
NabienneBintAhmad <mamelena.ndiongue@uvs.edu.sn>
2019-10-02 11:04:02 +0000

    tpken ok

---
+++ src/main/java/com/devweb/samaspringboot/SamaspringbootApplication.java
@@ -0,0 +1,14 @@
+package com.devweb.samaspringboot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class SamaspringbootApplication  {
+
+	public static void main(String[] args) {
+		run(SamaspringbootApplication.class, args);
+	}
+
+}
